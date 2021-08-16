758814f09d9a17f76ae9bb116ce7bba8f66628a6
Victor Song <vms2@rice.edu>
2021-08-15 20:16:33 -0500

    Initial project setup
     * Setup project with CRACO, ESLint, Prettier, etc.
    
     * Added React Router and Jest Styled Components

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,7 +1,8 @@
+/* eslint @typescript-eslint/no-floating-promises: "off" */
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
